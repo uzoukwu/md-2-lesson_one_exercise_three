44a5868177ea78c0f7f315c3de772a1053ae2aba
MaryWindows <anne.maggot@gmail.com>
2021-10-18 09:25:21 +0300

    Creating rectangle method Removing unused class fields Removing class constructor

--- app/src/main/java/com/example/lesson_one_exercise_three/AreaCalculator.java
+++ app/src/main/java/com/example/lesson_one_exercise_three/AreaCalculator.java
@@ -4,17 +4,6 @@ package com.example.lesson_one_exercise_three;
 import android.util.Log;
 
 public class AreaCalculator {
-    public int area;
-    public int height;
-    public int width;
-    public int base;
-
-    /*AreaCalculator(int area, int height, int base, int width) {
-        this.area = area;
-        this.base = base;
-        this.width = width;
-        this.height = height;
-    }*/
 
     public double circle(int radius) {
         return Log.d("myLog","Area of circle is: " + Math.PI * (radius * radius));
@@ -22,6 +11,10 @@ public class AreaCalculator {
 
     public double square(int area) {
         return Log.d("myLog","Area of square is: " + area * area);
+    }
+
+    public double rectangle(int width, int height){
+        return Log.d("myLog","Area of rectangle is: " + width * height);
     }
 
 
